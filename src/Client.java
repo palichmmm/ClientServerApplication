7bbf9e804ee2984b981f7cb33508541e12262381
Евгений Козловский <palichmmm@gmail.com>
2022-02-03 07:49:24 +0300

    Поправка решения

--- src/Client.java
+++ src/Client.java
@@ -8,10 +8,12 @@ public class Client {
 
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
-            out.println("Привет сервер, я клиент!");
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            out.println("Клиент!");
             String resp = in.readLine();
             System.out.println(resp);
+
         } catch (Exception err) {
             System.out.println(err);
         }
